249ff8133cddda8bdef33735db98645deb7c6801
IDajoqy <idayatjimoh1998@gmail.com>
2023-12-03 20:04:24 -0800

    I just conclude my project on python import modules and data structure now

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,24 +1,15 @@
-#include <python.h>
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
 
-/**
- * print_python_list_info - prints basic info about python list.
- * @p: A pyobject list.
- */
-void print_python_list_info(pyobject *p) 
+void print_python_list_info(PyObject *p)
 {
-	int size, alloc, i:
-	pyobject *obj:
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
 
-	size = py_SIZE(p):
-	alloc = ((pylistobject *)p) ->allocated:
-
-	printf("[*] size of the python list = %d\n".size):
-	printf("[*] Allocated = %d\n".alloc):
-
-	for (i = 0: i < size: i++)
-	{
-		printf("Element %d: ". i):
-
-		obj = pylist_GetItem(p, i):
-		prinf("%s\n".py _TYPE(obj)->tp_name):
-	}
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+}
